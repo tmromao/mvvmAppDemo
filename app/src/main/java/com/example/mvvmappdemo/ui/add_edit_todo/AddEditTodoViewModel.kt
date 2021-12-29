ed6d9ecd50139dbbce1634335db1b37830537aba
tromao <tiagomromao@gmail.com>
2021-12-29 20:38:07 +0000

    Building Add and edit todo Viewmodel

---
+++ app/src/main/java/com/example/mvvmappdemo/ui/add_edit_todo/AddEditTodoViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.mvvmappdemo.ui.add_edit_todo
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvmappdemo.data.Todo
+import com.example.mvvmappdemo.data.TodoRepository
+import com.example.mvvmappdemo.util.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddEditTodoViewModel @Inject constructor(
+    private val repository: TodoRepository,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    var todo by mutableStateOf<Todo?>(null)
+        private set
+
+    var title by mutableStateOf("")
+        private set
+
+    var description by mutableStateOf("")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    init {
+        val todoId = savedStateHandle.get<Int>("todoId")
+        if (todoId != -1) {
+            viewModelScope.launch {
+                todo = repository.getTodoById(todoId)
+                title = todo.title
+            }
+        }
+    }
+}
